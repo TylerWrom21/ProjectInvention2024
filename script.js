58023d46bdb7ce5053bd13265ee76b6d92d3771d
TylerWrom21 <tylergithubwrom.gmail.com>
2024-08-07 22:43:31 +0800

    Make The Login And Register Page

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 // theme
 let theme = document.querySelector('#theme');
 theme.onclick = () => {
-    if(theme) {
-        theme.classList.toggle('bxs-sun');
-        document.querySelector('body').classList.toggle('dark');
-    } else {
-        theme.classList.toggle('bxs-moon');
-    }
+    // if(theme) {
+    //     theme.classList.toggle('bxs-sun');
+    //     document.querySelector('body').classList.toggle('dark');
+    // } else {
+        //     theme.classList.toggle('bxs-moon');
+        // }
+    theme.classList.toggle('bxs-sun');
+    document.querySelector('body').classList.toggle('dark');
+}
+// menubar
+let menu = document.querySelector('#menu');
+menu.onclick = () => {
+    menu.classList.toggle('active');
+    menu.classList.toggle('bx-x');
+    document.querySelector('header').classList.toggle('active');
 }
